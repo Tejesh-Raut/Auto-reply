ba5af4e25b9a08bbd56d7071eb3fc94e4445fe1b
Tejesh Raut <tejesh22.raut@gmail.com>
2016-06-08 18:32:43 +0530

    Added some GUI interface

--- src/AutoReply.java
+++ src/AutoReply.java
@@ -1,13 +1,14 @@
 import java.awt.BorderLayout;
 import java.awt.Dimension;
-import java.awt.GridBagLayout;
 import java.awt.GridLayout;
-import java.io.BufferedReader;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.FileWriter;
-import java.io.InputStreamReader;
+import java.io.IOException;
 import java.io.PrintStream;
 import java.util.HashMap;
 import java.util.Iterator;
@@ -17,6 +18,7 @@ import java.util.Scanner;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
@@ -28,10 +30,10 @@ public class AutoReply
 {
 	public static void main (String Args[]) throws Exception
 	{
-		InputStreamReader read = new InputStreamReader(System.in);
-		BufferedReader in = new BufferedReader(read);
+		//InputStreamReader read = new InputStreamReader(System.in);
+		//BufferedReader in = new BufferedReader(read);
 		
-		int ch = 1;
+		//int ch = 1;
 
 		Map<String, String> query = new HashMap<String, String>();
 		Properties properties = new Properties();
@@ -45,7 +47,7 @@ public class AutoReply
 		JTextArea jTextArea = new JTextArea(20, 37);
 		JTextArea inputArea = new JTextArea(10, 37);
 		//jTextArea.append( "Hello World.-----------------------------------------------------" );
-		JFrame frame = new JFrame("Auto Reply selector");
+		final JFrame frame = new JFrame("Auto Reply selector");
 		frame.setVisible(true);
 		frame.setSize(1000,600);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -72,45 +74,322 @@ public class AutoReply
 		JButton button1 = new JButton("Ask a query");
 		button1.setPreferredSize(new Dimension(250, 50));
 		panel1.add(button1);
-		  //button.addActionListener (new Action1());
+		button1.setActionCommand("1");
 		
 		JButton button2 = new JButton("Display the entries in the map");
 		panel2.add(button2);
-		  //button.addActionListener (new Action2());
+		button2.setActionCommand("2");
 		
 		JButton button3 = new JButton("Add entries to map");
 		panel1.add(button3);
-		  //button.addActionListener (new Action1());
+		button3.setActionCommand("3");
 		
 		JButton button4 = new JButton("Load the map");
 		panel2.add(button4);
-		  //button.addActionListener (new Action2());
+		button4.setActionCommand("4");
 		
 		JButton button5 = new JButton("Save the map");
 		panel1.add(button5);
-		  //button.addActionListener (new Action1());
+		button5.setActionCommand("5");
 		
 		JButton button6 = new JButton("Delete an entry from hashmap");
 		panel2.add(button6);
-		  //button.addActionListener (new Action2());
+		button6.setActionCommand("6");
+		
 		JButton button7 = new JButton("Refresh the keywords");
 		panel1.add(button7);
-		  //button.addActionListener (new Action1());
+		button7.setActionCommand("7");
 		
 		JButton button8 = new JButton("Add new nonkeywords");
 		panel2.add(button8);
-		  //button.addActionListener (new Action2());
+		button8.setActionCommand("8");
+		
 		JButton button9 = new JButton("Add new synonyms");
 		panel1.add(button9);
-		  //button.addActionListener (new Action1());
+		button9.setActionCommand("9");
 		
 		JButton button10 = new JButton("Terminate the program");
 		panel2.add(button10);
-		  //button.addActionListener (new Action2());
+		button10.setActionCommand("10");
 		
+		button1.setEnabled(true);
+		button2.setEnabled(true);
+		button3.setEnabled(true);
+		button4.setEnabled(true);
+		button5.setEnabled(true);
+		button6.setEnabled(true);
+		button7.setEnabled(true);
+		button8.setEnabled(true);
+		button9.setEnabled(true);
+		button10.setEnabled(true);
 		
+		final Map<String, String> query1 = query;
+		button1.addActionListener(new ActionListener() 
+		{
+
+            @Override
+            public void actionPerformed(ActionEvent e)
+            {
+            	String q = JOptionPane.showInputDialog(null,"Enter your query: ");
+            	
+            	try 
+            	{
+					String q1 = BestMatch.BestQuery(q, query1);
+					JOptionPane.showMessageDialog (null, query1.get(q1), "Reply", JOptionPane.INFORMATION_MESSAGE);
+				} 
+            	catch (FileNotFoundException e1) 
+            	{
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}
+                
+            }
+
+        });
+		
+		button2.addActionListener(new ActionListener() 
+		{
+
+            @Override
+            public void actionPerformed(ActionEvent e)
+            {
+            	String reply ="";
+            	for (Map.Entry<String,String> entry : query1.entrySet()) 
+				{
+				    reply = reply + (entry.getKey()+"\t"+entry.getValue());
+				}
+            	JFrame frame = new JFrame("Reply");
+            	JTextArea textArea = new JTextArea(6, 25);
+                textArea.setText(reply);
+                textArea.setEditable(false);
+                 
+                // wrap a scrollpane around it
+                JScrollPane scrollPane = new JScrollPane(textArea);
+                 
+                // display them in a message dialog
+                JOptionPane.showMessageDialog(frame, scrollPane);
+            	//JOptionPane.showMessageDialog (null, reply, "Reply", JOptionPane.INFORMATION_MESSAGE);
+                
+            }
+
+        });
+		
+		final String[] NonKeywords1 = NonKeywords;
+		button3.addActionListener(new ActionListener() 
+		{
+
+            @Override
+            public void actionPerformed(ActionEvent e)
+            {
+				String q,r;
+				
+					r="";
+					
+					q = JOptionPane.showInputDialog(null,"Enter your query: ");
+					r = JOptionPane.showInputDialog(null,"Enter your reply: ");
+					
+					String[] allwords;
+					try {
+						allwords = KeepKeywords.SeparateWords(q);
+						String Keywords = KeepKeywords.Keywords(allwords, NonKeywords1);
+						query1.put(Keywords, r);
+						JOptionPane.showMessageDialog (null, "Entry added successfully", "Reply", JOptionPane.INFORMATION_MESSAGE);
+						//System.out.println("Entry added successfully");
+					} catch (Exception e1) {
+						// TODO Auto-generated catch block
+						e1.printStackTrace();
+					}
+                
+            }
+
+        });
+		
+		final Properties properties1 = properties;
+		button4.addActionListener(new ActionListener() 
+		{
+
+            @Override
+            public void actionPerformed(ActionEvent e)
+            {
+            	System.out.println("Loading the map ...");
+				{
+					try {
+						properties1.load(new FileInputStream("data.properties"));
+					} catch (FileNotFoundException e1) {
+						// TODO Auto-generated catch block
+						e1.printStackTrace();
+					} catch (IOException e1) {
+						// TODO Auto-generated catch block
+						e1.printStackTrace();
+					}
+	
+					for (String key : properties1.stringPropertyNames())
+					{
+					   query1.put(key, properties1.get(key).toString());
+					}
+				}
+				JOptionPane.showMessageDialog (null, "Loaded", "Reply", JOptionPane.INFORMATION_MESSAGE);
+				System.out.println("Loaded");
+            }
+
+        });
+		
+		button5.addActionListener(new ActionListener() 
+		{
+
+            @Override
+            public void actionPerformed(ActionEvent e)
+            {
+            	System.out.println("Saving the map ...");
+				{
+					for (Map.Entry<String,String> entry : query1.entrySet()) 
+					{
+					    properties1.put(entry.getKey(), entry.getValue());
+					}
+					try {
+						properties1.store(new FileOutputStream("data.properties"), null);
+					} catch (FileNotFoundException e1) {
+						// TODO Auto-generated catch block
+						e1.printStackTrace();
+					} catch (IOException e1) {
+						// TODO Auto-generated catch block
+						e1.printStackTrace();
+					}
+				}
+				JOptionPane.showMessageDialog (null, "Saved", "Reply", JOptionPane.INFORMATION_MESSAGE);
+				System.out.println("Saved");
+            }
+
+        });
+		
+		button6.addActionListener(new ActionListener() 
+		{
+
+            @Override
+            public void actionPerformed(ActionEvent e)
+            {
+				String q = JOptionPane.showInputDialog(null,"Enter the query to be deleted: ");
+				for(Iterator<Map.Entry<String, String>> it = query1.entrySet().iterator(); it.hasNext(); ) 
+				{
+				      Map.Entry<String, String> entry = it.next();
+				      if(entry.getKey().equals(q)) 
+				      {
+				        it.remove();
+				      }
+				}
+            }
+
+        });
+		
+		button7.addActionListener(new ActionListener() 
+		{
+
+            @Override
+            public void actionPerformed(ActionEvent e)
+            {
+            	System.out.println("Converting all existing queries to new Keywords");
+				String q,r;
+				Map<String, String> query2 = new HashMap<String, String>(query1);
+				for (Map.Entry<String,String> entry : query2.entrySet()) 
+				{
+					q=entry.getKey();
+					r=entry.getValue();
+					query1.remove(q);
+					String[] allwords;
+					try {
+						allwords = KeepKeywords.SeparateWords(q);
+						String Keywords = KeepKeywords.Keywords(allwords, NonKeywords1);
+						query1.put(Keywords, r);
+					} catch (Exception e1) {
+						// TODO Auto-generated catch block
+						e1.printStackTrace();
+					}
+				}
+				JOptionPane.showMessageDialog (null, "Keywords for queries refreshed successfully", "Reply", JOptionPane.INFORMATION_MESSAGE);
+				System.out.println("Keywords for queries refreshed successfully");
+            }
+
+        });
+		
+		final Scanner[] scanner1 = {scanner};
+		final String[] NonKeywordsString1 = {NonKeywordsString};
+		final String[][] NonKeywords2 = {NonKeywords1};
+		button8.addActionListener(new ActionListener() 
+		{
+            @Override
+            public void actionPerformed(ActionEvent e)
+            {
+				String w;
+				FileWriter fw;
+				try 
+				{
+					fw = new FileWriter("NonKeywords.txt",true);
+					w = JOptionPane.showInputDialog(null,"Enter a word to be added to non-keywords list: ");
+					fw.write(w+" ");
+					fw.close();
+				} 
+				catch (IOException e1) 
+				{
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}
+				
+				try 
+				{
+					scanner1[0] = new Scanner( new File("NonKeywords.txt"), "UTF-8" );
+					NonKeywordsString1[0] = scanner1[0].useDelimiter("\\A").next();
+					scanner1[0].close();
+					NonKeywords2[0] = NonKeywordsString1[0].split("\\s+");
+				} 
+				catch (FileNotFoundException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}
+				
+				
+            }
+
+        });
+		
+		button9.addActionListener(new ActionListener() 
+		{
+            @Override
+            public void actionPerformed(ActionEvent e)
+            {
+				FileWriter fw;
+				try 
+				{
+					fw = new FileWriter("Synonyms.txt",true);
+					String s = JOptionPane.showInputDialog(null,"Enter a word to be added to non-keywords list: ");
+					fw.write(s+"\n");
+					fw.close();
+				} 
+				catch (IOException e1) 
+				{
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}
+				
+            }
+
+        });
+		
+		button10.addActionListener(new ActionListener() 
+		{
+            @Override
+            public void actionPerformed(ActionEvent e)
+            {
+            	frame.dispose();
+            	//System.out.println("Closing the program .....");
+            	System.exit(0);
+            }
+
+        });
+		
+		/*
 		while(ch != 10)
 		{
+			//ch = Integer.parseInt(JOptionPane.showInputDialog(null, "Enter \n 1 to ask a query \n 2 to display the entries in the map \n 3 for adding entries to map \n 4 for loading the map \n 5 for saving the map \n 6 to delete an entry from hashmap \n 7 to refresh the keywords for queries \n 8 to add new nonkeywords \n 9 to add synonyms \n 10 to terminate the program "));
 			System.out.println("Enter \n 1 to ask a query \n 2 to display the entries in the map \n 3 for adding entries to map \n 4 for loading the map \n 5 for saving the map \n 6 to delete an entry from hashmap \n 7 to refresh the keywords for queries \n 8 to add new nonkeywords \n 9 to add synonyms \n 10 to terminate the program ");
 			ch = Integer.parseInt(in.readLine());
 			
@@ -198,8 +477,8 @@ public class AutoReply
 				{
 					System.out.println("Converting all existing queries to new Keywords");
 					String q,r;
-					Map<String, String> query1 = new HashMap<String, String>(query);
-					for (Map.Entry<String,String> entry : query1.entrySet()) 
+					Map<String, String> query2 = new HashMap<String, String>(query);
+					for (Map.Entry<String,String> entry : query2.entrySet()) 
 					{
 						q=entry.getKey();
 						r=entry.getValue();
@@ -258,8 +537,9 @@ public class AutoReply
 		}//end of while
 		
 		System.out.println("Program closed");
+		*/
 	}//end of main
-	
+	/*
 	public static void pressAnyKeyToContinue()
 	 { 
 	        System.out.println("Press any key to continue...");
@@ -270,4 +550,5 @@ public class AutoReply
 	        catch(Exception e)
 	        {}
 	 }
+	 */
 }//end of class AutoReply
