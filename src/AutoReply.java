935ffe0a2db1131d2c1dec55df3bd871791332fb
Tejesh Raut <tejesh22.raut@gmail.com>
2016-06-10 11:22:53 +0530

    GUI working fine

--- src/AutoReply.java
+++ src/AutoReply.java
@@ -9,7 +9,6 @@ import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.io.PrintStream;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Map;
@@ -17,7 +16,9 @@ import java.util.Properties;
 import java.util.Scanner;
 
 import javax.swing.JButton;
+import javax.swing.JDialog;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
@@ -43,74 +44,73 @@ public class AutoReply
 		scanner.close();
 		String[] NonKeywords = NonKeywordsString.split("\\s+");
 		
-		//System.out.println(NonKeywords);
-		JTextArea jTextArea = new JTextArea(20, 37);
-		JTextArea inputArea = new JTextArea(10, 37);
+		//*JTextArea jTextArea = new JTextArea(20, 37);
+		//*JTextArea inputArea = new JTextArea(10, 37);
 		//jTextArea.append( "Hello World.-----------------------------------------------------" );
 		final JFrame frame = new JFrame("Auto Reply selector");
 		frame.setVisible(true);
 		frame.setSize(1000,600);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		TextAreaOutputStream taOutputStream = new TextAreaOutputStream(jTextArea, "");
+		//*TextAreaOutputStream taOutputStream = new TextAreaOutputStream(jTextArea, "");
 
 	    
 		
 		JPanel panel1 = new JPanel(new GridLayout(5, 1, 10, 20));
 		JPanel panel2 = new JPanel(new GridLayout(5, 1, 10, 20));
-		JPanel panel3 = new JPanel(new BorderLayout());
+		//*JPanel panel3 = new JPanel(new BorderLayout());
 		JPanel panel = new JPanel(new BorderLayout(10,10));
 		panel.add(panel1, BorderLayout.WEST);
 		panel.add(panel2, BorderLayout.EAST);
-		panel.add(panel3, BorderLayout.CENTER);
+		//*panel.add(panel3, BorderLayout.CENTER);
 		frame.add(panel);
 		
 		
-		panel3.add(inputArea, BorderLayout.SOUTH);
-		panel3.add( jTextArea, BorderLayout.NORTH );
-		panel3.add(new JScrollPane(jTextArea, JScrollPane.VERTICAL_SCROLLBAR_ALWAYS,JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED));
+		//*panel3.add(inputArea, BorderLayout.SOUTH);
+		//*panel3.add( jTextArea, BorderLayout.NORTH );
+		//*panel3.add(new JScrollPane(jTextArea, JScrollPane.VERTICAL_SCROLLBAR_ALWAYS,JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED));
 		//panel3.add(new JScrollPane(inputArea, JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED,JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED));
-	      System.setOut(new PrintStream(taOutputStream));
+	    //*System.setOut(new PrintStream(taOutputStream));
 		
 		JButton button1 = new JButton("Ask a query");
 		button1.setPreferredSize(new Dimension(250, 50));
 		panel1.add(button1);
-		button1.setActionCommand("1");
+		//*button1.setActionCommand("1");
 		
 		JButton button2 = new JButton("Display the entries in the map");
 		panel2.add(button2);
-		button2.setActionCommand("2");
+		//*button2.setActionCommand("2");
 		
 		JButton button3 = new JButton("Add entries to map");
 		panel1.add(button3);
-		button3.setActionCommand("3");
+		//*button3.setActionCommand("3");
 		
 		JButton button4 = new JButton("Load the map");
 		panel2.add(button4);
-		button4.setActionCommand("4");
+		//*button4.setActionCommand("4");
 		
 		JButton button5 = new JButton("Save the map");
 		panel1.add(button5);
-		button5.setActionCommand("5");
+		//*button5.setActionCommand("5");
 		
 		JButton button6 = new JButton("Delete an entry from hashmap");
 		panel2.add(button6);
-		button6.setActionCommand("6");
+		//*button6.setActionCommand("6");
 		
 		JButton button7 = new JButton("Refresh the keywords");
 		panel1.add(button7);
-		button7.setActionCommand("7");
+		//*button7.setActionCommand("7");
 		
 		JButton button8 = new JButton("Add new nonkeywords");
 		panel2.add(button8);
-		button8.setActionCommand("8");
+		//*button8.setActionCommand("8");
 		
 		JButton button9 = new JButton("Add new synonyms");
 		panel1.add(button9);
-		button9.setActionCommand("9");
+		//*button9.setActionCommand("9");
 		
 		JButton button10 = new JButton("Terminate the program");
 		panel2.add(button10);
-		button10.setActionCommand("10");
+		//*button10.setActionCommand("10");
 		
 		button1.setEnabled(true);
 		button2.setEnabled(true);
@@ -123,15 +123,13 @@ public class AutoReply
 		button9.setEnabled(true);
 		button10.setEnabled(true);
 		
-		final Map<String, String> query1 = query;
+		final Map<String, String> query1 = query;//Create a final copy the map 'query'
 		button1.addActionListener(new ActionListener() 
 		{
-
-            @Override
+			@Override
             public void actionPerformed(ActionEvent e)
             {
             	String q = JOptionPane.showInputDialog(null,"Enter your query: ");
-            	
             	try 
             	{
 					String q1 = BestMatch.BestQuery(q, query1);
@@ -139,143 +137,150 @@ public class AutoReply
 				} 
             	catch (FileNotFoundException e1) 
             	{
-					// TODO Auto-generated catch block
+					JOptionPane.showMessageDialog (null, "File not found", "Reply", JOptionPane.INFORMATION_MESSAGE);
 					e1.printStackTrace();
 				}
-                
             }
-
         });
 		
 		button2.addActionListener(new ActionListener() 
 		{
-
             @Override
             public void actionPerformed(ActionEvent e)
             {
             	String reply ="";
             	for (Map.Entry<String,String> entry : query1.entrySet()) 
 				{
-				    reply = reply + (entry.getKey()+"\t"+entry.getValue());
+				    reply = reply +(entry.getKey()+"\n----------------------------------------------------------------------------------------------------\n"+entry.getValue()+"\n********************************************************************************\n\n\n");
 				}
             	JFrame frame = new JFrame("Reply");
-            	JTextArea textArea = new JTextArea(6, 25);
+            	JTextArea textArea = new JTextArea(40, 100);
+            	textArea.setLineWrap(true);
+            	textArea.setWrapStyleWord(true);
                 textArea.setText(reply);
                 textArea.setEditable(false);
-                 
                 // wrap a scrollpane around it
                 JScrollPane scrollPane = new JScrollPane(textArea);
-                 
                 // display them in a message dialog
                 JOptionPane.showMessageDialog(frame, scrollPane);
             	//JOptionPane.showMessageDialog (null, reply, "Reply", JOptionPane.INFORMATION_MESSAGE);
-                
             }
-
         });
 		
 		final String[] NonKeywords1 = NonKeywords;
 		button3.addActionListener(new ActionListener() 
 		{
-
-            @Override
+            @SuppressWarnings("static-access")
+			@Override
             public void actionPerformed(ActionEvent e)
             {
 				String q,r;
-				
-					r="";
-					
-					q = JOptionPane.showInputDialog(null,"Enter your query: ");
-					r = JOptionPane.showInputDialog(null,"Enter your reply: ");
-					
-					String[] allwords;
-					try {
-						allwords = KeepKeywords.SeparateWords(q);
-						String Keywords = KeepKeywords.Keywords(allwords, NonKeywords1);
-						query1.put(Keywords, r);
-						JOptionPane.showMessageDialog (null, "Entry added successfully", "Reply", JOptionPane.INFORMATION_MESSAGE);
-						//System.out.println("Entry added successfully");
-					} catch (Exception e1) {
-						// TODO Auto-generated catch block
-						e1.printStackTrace();
-					}
-                
+				q = JOptionPane.showInputDialog(null,"Enter your query: ");
+				r = new MyJOptionPane().showInputDialog("Enter your reply: ");
+				String[] allwords;
+				try 
+				{
+					allwords = KeepKeywords.SeparateWords(q);
+					String Keywords = KeepKeywords.Keywords(allwords, NonKeywords1);
+					query1.put(Keywords, r);
+					JOptionPane.showMessageDialog (null, "Entry added successfully", "Reply", JOptionPane.INFORMATION_MESSAGE);
+				} 
+				catch (Exception e1) 
+				{
+					e1.printStackTrace();
+				}
             }
-
         });
 		
 		final Properties properties1 = properties;
 		button4.addActionListener(new ActionListener() 
 		{
-
             @Override
             public void actionPerformed(ActionEvent e)
             {
-            	System.out.println("Loading the map ...");
 				{
-					try {
+					try 
+					{
 						properties1.load(new FileInputStream("data.properties"));
-					} catch (FileNotFoundException e1) {
-						// TODO Auto-generated catch block
-						e1.printStackTrace();
-					} catch (IOException e1) {
-						// TODO Auto-generated catch block
+					} 
+					catch (FileNotFoundException e1) 
+					{
+						JOptionPane.showMessageDialog (null, "File not found", "Reply", JOptionPane.INFORMATION_MESSAGE);
+					} 
+					catch (IOException e1) 
+					{
 						e1.printStackTrace();
 					}
-	
 					for (String key : properties1.stringPropertyNames())
 					{
 					   query1.put(key, properties1.get(key).toString());
 					}
 				}
 				JOptionPane.showMessageDialog (null, "Loaded", "Reply", JOptionPane.INFORMATION_MESSAGE);
-				System.out.println("Loaded");
             }
-
         });
 		
 		button5.addActionListener(new ActionListener() 
 		{
-
             @Override
             public void actionPerformed(ActionEvent e)
             {
-            	System.out.println("Saving the map ...");
 				{
 					for (Map.Entry<String,String> entry : query1.entrySet()) 
 					{
 					    properties1.put(entry.getKey(), entry.getValue());
 					}
-					try {
+					try 
+					{
 						properties1.store(new FileOutputStream("data.properties"), null);
-					} catch (FileNotFoundException e1) {
-						// TODO Auto-generated catch block
+					} 
+					catch (FileNotFoundException e1) 
+					{
 						e1.printStackTrace();
-					} catch (IOException e1) {
-						// TODO Auto-generated catch block
+					} 
+					catch (IOException e1) 
+					{
 						e1.printStackTrace();
 					}
 				}
 				JOptionPane.showMessageDialog (null, "Saved", "Reply", JOptionPane.INFORMATION_MESSAGE);
-				System.out.println("Saved");
             }
-
         });
 		
 		button6.addActionListener(new ActionListener() 
 		{
-
             @Override
             public void actionPerformed(ActionEvent e)
             {
 				String q = JOptionPane.showInputDialog(null,"Enter the query to be deleted: ");
-				for(Iterator<Map.Entry<String, String>> it = query1.entrySet().iterator(); it.hasNext(); ) 
-				{
-				      Map.Entry<String, String> entry = it.next();
-				      if(entry.getKey().equals(q)) 
-				      {
-				        it.remove();
-				      }
+				try 
+            	{
+					String q1 = BestMatch.BestQuery(q, query1);
+					int dialogButton = JOptionPane.YES_NO_OPTION;
+					int dialogResult = JOptionPane.showConfirmDialog (null, "Would You like to remove following entry: \n"+q1+"\n----------------------------------------------------------------------------------------------------\n"+query1.get(q1),"Warning",dialogButton);
+					if(dialogResult == 0) 
+					{
+						//System.out.println("Yes option");
+						for(Iterator<Map.Entry<String, String>> it = query1.entrySet().iterator(); it.hasNext(); ) 
+						{
+						      Map.Entry<String, String> entry = it.next();
+						      if(entry.getKey().equals(q1)) 
+						      {
+						        it.remove();
+						      }
+						}
+						JOptionPane.showMessageDialog (null, "Entry deleted", "Reply", JOptionPane.INFORMATION_MESSAGE);
+					} 
+					else 
+					{
+						//System.out.println("No Option");
+						JOptionPane.showMessageDialog (null, "Entry not deleted", "Reply", JOptionPane.INFORMATION_MESSAGE);
+					} 
+				} 
+            	catch (FileNotFoundException e1) 
+            	{
+					JOptionPane.showMessageDialog (null, "File not found", "Reply", JOptionPane.INFORMATION_MESSAGE);
+					e1.printStackTrace();
 				}
             }
 
@@ -283,11 +288,9 @@ public class AutoReply
 		
 		button7.addActionListener(new ActionListener() 
 		{
-
             @Override
             public void actionPerformed(ActionEvent e)
             {
-            	System.out.println("Converting all existing queries to new Keywords");
 				String q,r;
 				Map<String, String> query2 = new HashMap<String, String>(query1);
 				for (Map.Entry<String,String> entry : query2.entrySet()) 
@@ -296,19 +299,19 @@ public class AutoReply
 					r=entry.getValue();
 					query1.remove(q);
 					String[] allwords;
-					try {
+					try 
+					{
 						allwords = KeepKeywords.SeparateWords(q);
 						String Keywords = KeepKeywords.Keywords(allwords, NonKeywords1);
 						query1.put(Keywords, r);
-					} catch (Exception e1) {
-						// TODO Auto-generated catch block
+					} 
+					catch (Exception e1) 
+					{
 						e1.printStackTrace();
 					}
 				}
 				JOptionPane.showMessageDialog (null, "Keywords for queries refreshed successfully", "Reply", JOptionPane.INFORMATION_MESSAGE);
-				System.out.println("Keywords for queries refreshed successfully");
             }
-
         });
 		
 		final Scanner[] scanner1 = {scanner};
@@ -327,10 +330,10 @@ public class AutoReply
 					w = JOptionPane.showInputDialog(null,"Enter a word to be added to non-keywords list: ");
 					fw.write(w+" ");
 					fw.close();
+					JOptionPane.showMessageDialog (null, "Nonkeyword added successfully", "Reply", JOptionPane.INFORMATION_MESSAGE);
 				} 
 				catch (IOException e1) 
 				{
-					// TODO Auto-generated catch block
 					e1.printStackTrace();
 				}
 				
@@ -341,14 +344,12 @@ public class AutoReply
 					scanner1[0].close();
 					NonKeywords2[0] = NonKeywordsString1[0].split("\\s+");
 				} 
-				catch (FileNotFoundException e1) {
-					// TODO Auto-generated catch block
+				catch (FileNotFoundException e1) 
+				{
+					JOptionPane.showMessageDialog (null, "File not found", "Reply", JOptionPane.INFORMATION_MESSAGE);
 					e1.printStackTrace();
 				}
-				
-				
             }
-
         });
 		
 		button9.addActionListener(new ActionListener() 
@@ -360,18 +361,15 @@ public class AutoReply
 				try 
 				{
 					fw = new FileWriter("Synonyms.txt",true);
-					String s = JOptionPane.showInputDialog(null,"Enter a word to be added to non-keywords list: ");
+					String s = JOptionPane.showInputDialog(null,"Enter synonyms separated by spaces: ");
 					fw.write(s+"\n");
 					fw.close();
 				} 
 				catch (IOException e1) 
 				{
-					// TODO Auto-generated catch block
 					e1.printStackTrace();
 				}
-				
             }
-
         });
 		
 		button10.addActionListener(new ActionListener() 
@@ -380,10 +378,8 @@ public class AutoReply
             public void actionPerformed(ActionEvent e)
             {
             	frame.dispose();
-            	//System.out.println("Closing the program .....");
             	System.exit(0);
             }
-
         });
 		
 		/*
@@ -552,3 +548,43 @@ public class AutoReply
 	 }
 	 */
 }//end of class AutoReply
+@SuppressWarnings("serial")
+class MyJOptionPane extends JOptionPane
+{
+  public static String showInputDialog(final String message)
+  {
+    String data = null;
+    class GetData extends JDialog implements ActionListener
+    {
+      JTextArea ta = new JTextArea(10,50);
+      JButton btnOK = new JButton("   OK   ");
+      JButton btnCancel = new JButton("Cancel");
+      String str = null;
+      public GetData()
+      {
+        setModal(true);
+        getContentPane().setLayout(new BorderLayout());
+        setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+        setLocation(400,300);
+        getContentPane().add(new JLabel(message),BorderLayout.NORTH);
+        getContentPane().add(ta,BorderLayout.CENTER);
+        JPanel jp = new JPanel();
+        btnOK.addActionListener(this);
+        btnCancel.addActionListener(this);
+        jp.add(btnOK);
+        jp.add(btnCancel);
+        getContentPane().add(jp,BorderLayout.SOUTH);
+        pack();
+        setVisible(true);
+      }
+      public void actionPerformed(ActionEvent ae)
+      {
+        if(ae.getSource() == btnOK) str = ta.getText();
+        dispose();
+      }
+      public String getData(){return str;}
+    }
+    data = new GetData().getData();
+    return data;
+  }
+}
